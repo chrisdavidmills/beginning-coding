5b431b9456f26889f9281b49de782c5978e33e54
Chris Mills <cmills@opera.com>
2014-02-12 09:55:12 +0000

    initial population of the repo

---
+++ server-side-vs-client-side/php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A PHP page</title>
+	</head>
+
+	<body>
+    <h1>This is PHP: the current date and time is
+    <?php print strftime('%c');?>
+    </h1>
+	</body>
+</html>
